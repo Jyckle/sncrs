bbe520ba13265f650e34492f3c58046f60ff5b4a
Jyckle <jycklepasquesi@gmail.com>
2026-04-03 23:13:18 -0500

    feat: switch user

--- sncrs/start_server.sh
+++ sncrs/start_server.sh
@@ -8,10 +8,10 @@ done
 
 echo 'PostgreSQL started'
 
-(cd /sncrs/; sudo -u www-data python manage.py collectstatic --no-input; sudo -u www-data python manage.py makemigrations --no-input; sudo -u www-data python manage.py migrate --no-input)
+(cd /sncrs/; python manage.py collectstatic --no-input; python manage.py makemigrations --no-input; python manage.py migrate --no-input)
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd /sncrs/; sudo -u www-data python manage.py createsuperuser --no-input)
+    (cd /sncrs/; python manage.py createsuperuser --no-input)
 fi
 
 reload_args=()
@@ -19,5 +19,5 @@ if [ "$LIVE_RELOAD" = "true" ]; then
     reload_args=('--reload')
 fi
 
-(cd /sncrs/; gunicorn sncrs.wsgi ${reload_args[@]} --timeout 1200 --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd /sncrs/; gunicorn sncrs.wsgi ${reload_args[@]} --timeout 1200 --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
