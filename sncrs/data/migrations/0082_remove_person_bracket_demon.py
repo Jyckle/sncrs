86d79cdddd05bed4634b2f180c510a9f8394d453
coltatgh <clewis01038@utexas.edu>
2026-06-30 09:46:51 -0700

    Remove single shot scripts and migrations

--- sncrs/data/migrations/0082_remove_person_bracket_demon.py
+++ sncrs/data/migrations/0082_remove_person_bracket_demon.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data', '0082_backfill_challenge_match_game_titles'),
+        ('data', '0081_backfill_match_game_titles'),
     ]
 
     operations = [
